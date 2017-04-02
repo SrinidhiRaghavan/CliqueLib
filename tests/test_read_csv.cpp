53b5859f2250413390b311042aae618566be0fc2
skp2140 <skp2140@columbia.edu>
2017-04-01 21:06:23 -0400

    Test cases for read_csv() added, need to create sample files

--- tests/test_read_csv.cpp
+++ tests/test_read_csv.cpp
@@ -13,23 +13,39 @@ void test_read_csv()
 
     //Test 1
     std::cout << "\nTESTING READING CSV FILE 1" << std::endl;
-    read_csv("./samples_csv/origin_wine.csv", true);
+    read_csv("./samples_csv/test_file_1_title_true.csv", true);
     
     //Test 2
     std::cout << "\nTESTING READING CSV FILE 2" << std::endl;
-    read_csv("test_file_2.csv");
+    read_csv("./samples_csv/test_file_2_title_false.csv", false);
 
     //Test 3
     std::cout << "\nTESTING READING CSV FILE 3" << std::endl;
-    read_csv("test_file_3.csv");
+    read_csv("./samples_csv/test_file_3_title_default.csv");
 
     //Test 4
     std::cout << "\nTESTING READING CSV FILE 4" << std::endl;
-    read_csv("test_file_4.csv");
+    read_csv("./samples_csv/test_file_4_separate_ex_space.csv", ' ');
 
     //Test 5
     std::cout << "\nTESTING READING CSV FILE 5" << std::endl;
-    read_csv("test_file_5.csv");
+    read_csv("./samples_csv/test_file_5_separate_ex_semicolon.csv", ';');
+
+    //Test 6
+    std::cout << "\nTESTING READING CSV FILE 6" << std::endl;
+    read_csv("./samples_csv/test_file_6_separate_ex_default.csv");
+
+    //Test 7
+    std::cout << "\nTESTING READING CSV FILE 7" << std::endl;
+    read_csv("./samples_csv/test_file_7_comment_*.csv", ',', '*');
+
+    //Test 8
+    std::cout << "\nTESTING READING CSV FILE 8" << std::endl;
+    read_csv("./samples_csv/test_file_8_comment_default.csv", ',');
+
+    //Test 9
+    std::cout << "\nTESTING READING CSV FILE 9" << std::endl;
+    read_csv("./samples_csv/test_file_9_origin_wine.csv", true);
     
 }
 
