4bcdee7b287436df1a5a5370bec88aa7a585f4cb
Aliga8or <ali786.p@gmail.com>
2017-04-13 05:32:30 -0400

    calculateThreshold for decision stub implemented with armadillo support

--- source/AdaBoost/src/AdaBoost.cpp
+++ source/AdaBoost/src/AdaBoost.cpp
@@ -1,9 +1,9 @@
 //============================================================================
-// Name        : svm.cpp
+// Name        : AdaBoost.cpp
 // Author      : 
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : CliqueLib AdaBoost implementation
 //============================================================================
 
 #include <iostream>
@@ -12,13 +12,71 @@
 using namespace std;
 using namespace arma;
 
+void calculateThreshold(const colvec& x, const colvec& y, const colvec& w, const string& sign) {
+	uword N = x.size();
+	cout << "N: " << N << endl;
+	
+	colvec err_n, y_predict;
+	err_n.zeros(N, 1);
+	y_predict.zeros(N, 1);
+	uvec idx;
+
+	for (unsigned int i = 0; i < N; i++) {
+		if (sign == "greater")
+		{
+			cout << ">" << endl;
+			idx = find(x >= x(i));
+			cout << "idx:" << idx << endl;
+			y_predict.fill(-1);
+			y_predict.elem(idx).ones();
+			cout << "y_predict:" << y_predict << endl;
+		}
+		else
+		{
+			cout << "<" << endl;
+			idx = find(x <= x(i));
+			cout << "idx:" << idx << endl;
+			y_predict.fill(-1);
+			y_predict.elem(idx).ones();
+			cout << "y_predict:" << y_predict << endl;
+		}
+		cout << "y.size():" << y.size() << endl;
+		cout << "y_predict.size():" << y_predict.size() << endl;
+		umat err_label = (y != y_predict);
+		cout << "err_label:" << err_label << endl;
+		err_n(i) = accu(err_label);
+		cout << "sum:" << accu(err_label) << endl;
+	}
+	cout << "before err_n:" << err_n << endl;
+	err_n = err_n / accu(w);
+	cout << "after err_n:" << err_n << endl;
+	double error = min(err_n);
+	uword min_idx = index_min(err_n);
+	double thres = x(min_idx);
+	cout << "Min error:" << error << " found at idx:" << min_idx << " for thres:" << thres << endl;
+}
+
 int main()
 {
-	mat A = randu<mat>(4, 5);
-	mat B = randu<mat>(4, 5);
+	colvec x;
+	x << 1.0 << endr
+	  << 2.0 << endr
+	  << 3.0 << endr;
 
-	cout << "Result:" << endl;
-	cout << A*B.t() << endl;
+	uword N = x.size();
+	colvec w;
+	w.ones(N, 1);
+	
+	colvec y;
+	y << 1.0 << endr 
+	  << -1.0 << endr
+	  << 1.0 << endr;
+
+	cout << "x:" << x << endl;
+	cout << "y:" << y << endl;
+	
+	calculateThreshold(x, y, w, "lesser");
+	
 	getchar();
 	return 0;
 }
