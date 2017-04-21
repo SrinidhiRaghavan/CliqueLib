ccb71d2fda1684e444a9913867b89f22096269ad
skp2140 <skp2140@columbia.edu>
2017-04-20 21:22:07 -0400

    Tests for KNN refactored

--- tests/test_knn.cpp
+++ tests/test_knn.cpp
@@ -24,6 +24,7 @@ void test_knn()
     assert(knn1.train() == train_file);
     vector<float> class_ex1 = {3, 9};
     knn1.classify(class_ex1);
+    cout << "\nKNN Test 1 passed\n";
 
     //Test 2
     std::cout << "\nTESTING KNN CSV FILE 2" << std::endl;
@@ -35,6 +36,7 @@ void test_knn()
     assert(knn2.train() == train_file);
     vector<float> class_ex2 = {3, 7.4};
     knn2.classify(class_ex2);
+    cout << "\nKNN Test 2 passed\n";
 
     //Test 3
     std::cout << "\nTESTING KNN CSV FILE 3" << std::endl;
@@ -46,6 +48,7 @@ void test_knn()
     assert(knn3.train() == train_file);
     vector<float> class_ex3 = {3, 7.4, 2};
     knn3.classify(class_ex3);
+    cout << "\nKNN Test 3 passed\n";
 
     //Test 4
     std::cout << "\nTESTING KNN CSV FILE 4" << std::endl;
@@ -57,6 +60,7 @@ void test_knn()
     assert(knn4.train() == train_file);
     vector<float> class_ex4 = {3, 7.4};
     knn4.classify(class_ex4);
+    cout << "\nKNN Test 4 passed\n";
 
     //Test 5
     std::cout << "\nTESTING KNN CSV FILE 5" << std::endl;
@@ -68,6 +72,7 @@ void test_knn()
     assert(knn5.train() == train_file);
     vector<float> class_ex5 = {3, 7.4};
     knn5.classify(class_ex5);
+    cout << "\nKNN Test 5 passed\n";
 
     //Test 6
     std::cout << "\nTESTING KNN CSV FILE 6" << std::endl;
@@ -79,6 +84,7 @@ void test_knn()
     assert(knn6.train() == train_file);
     vector<float> class_ex6 = {3, 7.4, 2, 3};
     knn6.classify(class_ex6);
+    cout << "\nKNN Test 6 passed\n";
 
     //Test 7
     std::cout << "\nTESTING KNN CSV FILE 7" << std::endl;
@@ -90,6 +96,7 @@ void test_knn()
     assert(knn7.train() == train_file);
     vector<float> class_ex7 = {3, 7.4, 8, 4};
     knn7.classify(class_ex7);
+    cout << "\nKNN Test 7 passed\n";
 
     //Test 8
     std::cout << "\nTESTING KNN CSV FILE 8" << std::endl;
@@ -101,6 +108,7 @@ void test_knn()
     assert(knn8.train() == train_file);
     vector<float> class_ex8 = {3, 7.4, 2, 3};
     knn8.classify(class_ex8);
+    cout << "\nKNN Test 8 passed\n";
 
     //Test 9
     std::cout << "\nTESTING KNN CSV FILE 9" << std::endl;
@@ -112,6 +120,7 @@ void test_knn()
     assert(knn9.train() == train_file);
     vector<float> class_ex9 = {3, 7.4, 3, 4.4};
     knn9.classify(class_ex9);
+    cout << "\nKNN Test 9 passed\n";
 
     //Test 10
     std::cout << "\nTESTING KNN CSV FILE 10" << std::endl;
@@ -123,15 +132,19 @@ void test_knn()
     assert(knn10.train() == train_file);
     vector<float> class_ex10 = {3, 7.4};
     knn10.classify(class_ex10);
+    cout << "\nKNN Test 10 passed\n";
 
     //Test 11
+    std::cout << "\nTESTING KNN EUCLIDEAN DISTANCE 11" << std::endl;
     KNN knn11 (train_file, 2);
     vector<float> examp1 = {2, 2, 2, 2, 1};
     vector<float> examp2 = {4, 4, 4, 4, 0};
     assert(knn11.euclideanDistance(4, examp1, examp2) == 4.0);
+    cout << "\nKNN Test 11 passed\n";
 
     //Test 12
     //machinelearningmastery.com
+    std::cout << "\nTESTING KNN FIND CLASS 12" << std::endl;
     KNN knn12 (train_file, 3);
     vector<vector<float> > neighb;
     vector<float> subn1 = {1,1,1,0};
@@ -141,6 +154,7 @@ void test_knn()
     neighb.push_back(subn2);
     neighb.push_back(subn3);
     assert(knn12.findClass(neighb) == 0.0);
+    cout << "\nKNN Test 12 passed\n";
 
 }
 
