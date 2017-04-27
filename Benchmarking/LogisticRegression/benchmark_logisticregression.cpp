5723ab1ac83a8b1364131caf7dfbe035caa7b332
skp2140 <skp2140@columbia.edu>
2017-04-27 02:15:24 -0400

    Benchmarking for logistic regression done, CliqueLib yields very good results

--- Benchmarking/LogisticRegression/benchmark_logisticregression.cpp
+++ Benchmarking/LogisticRegression/benchmark_logisticregression.cpp
@@ -7,116 +7,62 @@
 //============================================================================
 
 #include <iostream>
-#include <memory>
 #include <armadillo>
 #include <vector>
-#include <cassert>
-#include <cmath>
-#include <utility>
+#include <chrono>
 #include "preprocessing_functions.h"
-#include "Dataset.h"
 #include "logistic_regression.h"
 #include "logistic_regression_std.h"
 
 using namespace arma;
 
-double getAccuracy(colvec& a, colvec& b) {
-    return double(accu(a==b))/size(a, 0);
-}
+/*
+Sample Output:
+
+Time with CliqueLib's Armadillo Implementation: 25744 microseconds.
+Time with CliqueLib's Vector Implementation: 63410 microseconds.
+
+*/
 
 int main()
 {
-/*
-    //use case 2
+    //CliqueLib Armadillo Implementation
+
     mat X;
     colvec Y;
-    //read_csv("samples_csv/toy_sample.csv", X, Y, true, ',', '#');
-    //read_csv("samples_csv/admits.csv", X, Y, false, ',', '#');
     read_csv("../../samples_csv/Cancer.csv", X, Y, true, ',', '#');
-    cout << "size X:" << arma::size(X) << endl;
-    cout << "size Y:" << arma::size(Y) << endl;
-    //cout << "X:" << X.rows(0, 4) << endl;
-    cout << "Y:" << Y.rows(0, 4) << endl;
-    
-    Dataset data;
-    split_train_test(X, Y, data, 0.8);
-    cout << "size Xtrain:" << arma::size(data.Xtrain) << endl;
-    cout << "size Ytrain:" << arma::size(data.Ytrain) << endl;
-    cout << "size Xtest:" << arma::size(data.Xtest) << endl;
-    cout << "size Ytest:" << arma::size(data.Ytest) << endl;
-    
-    AdaBoost clfr;
-    clfr.train(data.Xtrain, data.Ytrain, 100);
-    colvec preds;
-    clfr.predict(data.Xtest, preds);
 
-    auto acc = getAccuracy(data.Ytest, preds);
-    cout << "AdaBoost acc:" << acc << endl;
-
-    KNN knn_clfr;
-    knn_clfr.train(data.Xtrain, data.Ytrain, 100);
-    colvec preds_knn;
-    knn_clfr.predict(data.Xtest, preds_knn);
-
-    auto acc_knn = getAccuracy(data.Ytest, preds_knn);
-    cout << "KNN acc:" << acc_knn << endl;
-   
-    
-    SVM svm_clfr;
-    svm_clfr.train(data.Xtrain, data.Ytrain, 100);
-    colvec preds_svm;
-    svm_clfr.predict(data.Xtest, preds_svm);
-
-    auto acc_svm = getAccuracy(data.Ytest, preds_svm);
-    cout << "SVM acc:" << acc_svm << endl;
-
-    Perceptron pcptr_clfr;
-    pcptr_clfr.train(data.Xtrain, data.Ytrain, 100);
-    colvec preds_pcptr;
-    pcptr_clfr.predict(data.Xtest, preds_pcptr);
-
-    auto acc_pcptr = getAccuracy(data.Ytest, preds_pcptr);
-    cout << "Perceptron acc:" << acc_pcptr << endl;
-    
-    BaggingClassifier<AdaBoost> bgClfr(5, 400);
-    bgClfr.train(data.Xtrain, data.Ytrain, 100);
-    colvec preds_bg;
-    bgClfr.predict(data.Xtest, preds_bg);
-
-    auto acc_bg = getAccuracy(data.Ytest, preds_bg);
-    cout << "Bg adB acc:" << acc_bg << endl;
-    
-    BaggingClassifier<SVM> bgClfr_svm(5, 400);
-    bgClfr_svm.train(data.Xtrain, data.Ytrain, 100);
-    colvec preds_bg_svm;
-    bgClfr_svm.predict(data.Xtest, preds_bg_svm);
-
-    auto acc_bg_svm = getAccuracy(data.Ytest, preds_bg_svm);
-    cout << "Bg svm acc:" << acc_bg_svm << endl;
-    
-
+    //Logistic Regression with learning rate == 1 & number of epochs == 100
+    //takes on average  25000 microseconds on a very slow computer
+    std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();    
     Logistic logistic_clfr(0.01);
-    logistic_clfr.train(data.Xtrain, data.Ytrain, 100);
+    logistic_clfr.train(X, Y, 100);
     colvec preds_log;
-    logistic_clfr.predict(data.Xtest, preds_log);
-    auto acc_log = getAccuracy(data.Ytest, preds_log);
-    cout << "Logistic acc: " << acc_log << endl;	
+    logistic_clfr.predict(X, preds_log);
 
-    VotingClassifier vcClfr;
-    auto svm_clfr_b = make_shared<SVM>(svm_clfr);
-    auto pcptr_clfr_b = make_shared<Perceptron>(pcptr_clfr);
-    auto logistic_clfr_b = make_shared<Logistic>(logistic_clfr);
-    
-    vcClfr.addClassifier(svm_clfr_b);
-    vcClfr.addClassifier(pcptr_clfr_b);
-    vcClfr.addClassifier(logistic_clfr_b);
-    
-    vcClfr.train(data.Xtrain, data.Ytrain, 100);
-    colvec preds_vc;
-    vcClfr.predict(data.Xtest, preds_vc);
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    cout << "Time with CliqueLib's Armadillo Implementation: " 
+         << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
+         << " microseconds." << endl;
 
-    auto acc_vc = getAccuracy(data.Ytest, preds_vc);
-    cout << "Vc acc:" << acc_vc << endl;
-*/
+    //CliqueLib Vector Implementation
+
+    vector<vector<float> > test_file = read_csv("../../samples_csv/Cancer.csv",
+	                                        true, ',', '#');
+    vector<vector<float> > train_file = test_file;
+    for (auto& instance : test_file)    
+        instance[instance.size() - 1] = 0;
+
+    //Logistic Regression with learning rate == 1 & number of epochs == 100
+    //takes on average 64000 microseconds on a very slow computer
+    start = std::chrono::steady_clock::now();
+    LogisticRegressionStd log_vec (train_file, 100, 0.01);
+    vector<float> coefficients = log_vec.train();
+    log_vec.predict(test_file, coefficients);
+    end = std::chrono::steady_clock::now();
+    cout << "Time with CliqueLib's Vector Implementation: " 
+         << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
+         << " microseconds." << endl;
+
     return 0;
 }
