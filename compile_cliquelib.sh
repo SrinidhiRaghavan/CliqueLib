afd1ad98d7fb678b27a2de504946ff9e3a05c7ed
skp2140 <skp2140@columbia.edu>
2017-04-27 00:36:20 -0400

    Script compile_cliquelib.sh modified to include Logistic Regression Benchmarking

--- compile_cliquelib.sh
+++ compile_cliquelib.sh
@@ -2,12 +2,19 @@
 #compile_cliquelib.sh
 #Script to compile the library and the use cases
 set -e
+#compile all library's algorithms and run tests
 make -f ./Makefile
+#compile benchmarking for KNN
 cd Benchmarking/KNN/
 make -f ./Makefile
+#compile benchmarking for Logistic Regression
+cd ../LogisticRegression/
+make -f ./Makefile
+#compile use case 1
 cd ../..
 cd UseCases/UseCase1/
 make -f ./Makefile
+#compile use case 2
 cd ../UseCase2
 make -f ./Makefile
 cd ../..
