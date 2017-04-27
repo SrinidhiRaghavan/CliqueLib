2c8983425c47d33ce2a635f2d9058748d16e782d
skp2140 <skp2140@columbia.edu>
2017-04-27 19:20:19 -0400

    Bagging Classifier format refined

--- source/BaggingClassifier/BaggingClassifier.cpp
+++ source/BaggingClassifier/BaggingClassifier.cpp
@@ -7,8 +7,7 @@
 //============================================================================
 
 #include <iostream>
-#include <armadillo>
-#include <math.h>
+#include <cmath>
 #include "AdaBoost.h"
 #include "KNN.h"
 #include "SVM.h"
@@ -21,45 +20,45 @@ using namespace arma;
 template<Classifier C>
 BaggingClassifier<C>::BaggingClassifier(uword n_estimators, uword max_samples) 
 {
-	this->n_estimators = n_estimators;
-	this->max_samples = max_samples;
+    this->n_estimators = n_estimators;
+    this->max_samples = max_samples;
 }
 
 template<Classifier C>
 void BaggingClassifier<C>::train(const mat& X, const colvec& Y, uword iter) 
 {
-	uword n = size(X, 0);
+    uword n = size(X, 0);
 
-	for (uword i = 0; i < n_estimators; i++) 
-        {
-		uvec randIdx = randi<uvec>(max_samples, 1, distr_param(0, n-1));
-		auto clfr = make_shared<C>();
-		clfr->train(X.rows(randIdx), Y.rows(randIdx), iter);
-		base_estimators.push_back(clfr);
-	}
+    for (uword i = 0; i < n_estimators; i++) 
+    {
+	uvec randIdx = randi<uvec>(max_samples, 1, distr_param(0, n-1));
+	auto clfr = make_shared<C>();
+	clfr->train(X.rows(randIdx), Y.rows(randIdx), iter);
+	base_estimators.push_back(clfr);
+    }
 }
 
 template<Classifier C>
 void BaggingClassifier<C>::predict(const mat& testX, colvec& labels) 
 {
-	uword n = size(testX, 0);
-	mat predMat;
-	predMat.zeros(n, n_estimators);
-	labels.zeros(n, 1);
+    uword n = size(testX, 0);
+    mat predMat;
+    predMat.zeros(n, n_estimators);
+    labels.zeros(n, 1);
 
-	auto baseIter = base_estimators.begin();
-	for (uword i = 0; baseIter != base_estimators.end(); baseIter++, i++) 
-        {
-		auto clfr = *baseIter;
-		colvec preds;
-		preds.zeros(n, 1);
-		clfr->predict(testX, preds);
-		predMat.col(i) = preds;
-	}
+    auto baseIter = base_estimators.begin();
+    for (uword i = 0; baseIter != base_estimators.end(); baseIter++, i++) 
+    {
+	auto clfr = *baseIter;
+	colvec preds;
+	preds.zeros(n, 1);
+	clfr->predict(testX, preds);
+	predMat.col(i) = preds;
+    }
 
-	uvec idx = find(mean(predMat, 1) > 0);
-	labels.fill(-1);
-	labels.elem(idx).fill(1);
+    uvec idx = find(mean(predMat, 1) > 0);
+    labels.fill(-1);
+    labels.elem(idx).fill(1);
 }
 
 template class BaggingClassifier<AdaBoost>;
