b78e3f7a565e1eceaf1fd2200b1560b1dc2751fe
skp2140 <skp2140@columbia.edu>
2017-04-27 00:10:47 -0400

    Benchmarking KNN added to the compile_cliqelib.sh script

--- compile_cliquelib.sh
+++ compile_cliquelib.sh
@@ -3,6 +3,9 @@
 #Script to compile the library and the use cases
 set -e
 make -f ./Makefile
+cd Benchmarking/KNN/
+make -f ./Makefile
+cd ../..
 cd UseCases/UseCase1/
 make -f ./Makefile
 cd ../UseCase2
