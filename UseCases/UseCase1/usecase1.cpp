244ed264214415041e55dfe4ac76825fa7853583
Aliga8or <ali786.p@gmail.com>
2017-04-27 04:34:58 +0000

    Modified and refined face detection AdaBoost

--- UseCases/UseCase1/usecase1.cpp
+++ UseCases/UseCase1/usecase1.cpp
@@ -13,7 +13,7 @@
 #include <cmath>
 #include <utility>
 #include "preprocessing_functions.h"
-#include "Stump.h"
+#include "Dataset.h"
 #include "AdaBoost.h"
 
 using namespace arma;
@@ -28,28 +28,18 @@ int main()
 {
     mat X;
     colvec Y;
-    //read_csv("samples_csv/toy_sample.csv", X, Y, true, ',', '#');
-    //read_csv("samples_csv/admits.csv", X, Y, false, ',', '#');
     read_csv("../../samples_csv/face_detection.csv", X, Y, true, ',', '#');
-    cout << "size X:" << arma::size(X) << endl;
-    cout << "size Y:" << arma::size(Y) << endl;
-    //cout << "X:" << X.rows(0, 4) << endl;
-    cout << "Y:" << Y.rows(0, 4) << endl;
-    mat Xtrain = X.rows(0, 1599);
-    colvec Ytrain = Y.rows(0, 1599);
-    mat Xtest = X.rows(1600, 1998);
-    colvec Ytest = Y.rows(1600, 1998);
-    /*BaggingClassifier<SVM> bgClfr_svm(10, 1200);
-    bgClfr_svm.train(Xtrain, Ytrain, 100);
-    colvec preds_bg_svm;
-    bgClfr_svm.predict(Xtest, preds_bg_svm);
-    auto acc_bg_svm = getAccuracy(Ytest, preds_bg_svm);
-    cout << "Bg svm acc:" << acc_bg_svm << endl;*/
+    X = X.rows(0, 999);
+    Y = Y.rows(0, 999);
+    
+    Dataset data;
+    split_train_test(X, Y, data, 0.6);
+    
     AdaBoost clfr;
-    clfr.train(Xtrain, Ytrain, 100);
+    clfr.train(data.Xtrain, data.Ytrain, 5);
     colvec preds;
-    clfr.predict(Xtest, preds);
-    auto acc = getAccuracy(Ytest, preds);
+    clfr.predict(data.Xtest, preds);
+    auto acc = getAccuracy(data.Ytest, preds);
     cout << "AdaBoost acc:" << acc << endl;
 
     return 0;
