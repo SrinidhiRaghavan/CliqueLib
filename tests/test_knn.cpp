1e24125a0bfa88ac0ab287c9ebcbe1c721fa9af8
skp2140 <skp2140@columbia.edu>
2017-04-28 00:01:44 -0400

    Tests for KNN were added

--- tests/test_knn.cpp
+++ tests/test_knn.cpp
@@ -10,6 +10,17 @@
 
 void test_knn()
 {
+    mat X;
+    colvec Y;
+    read_csv("samples_csv/linear.csv", X, Y, true, ',', '#');
 
+    std::cout << "\nTESTING KNN" << std::endl;
+    Dataset data;
+    split_train_test(X, Y, data, 0.8);
+    KNN knn_clfr;
+    knn_clfr.train(data.Xtrain, data.Ytrain, 100);
+    colvec preds_knn;
+    knn_clfr.predict(data.Xtest, preds_knn);
+    std::cout << "\nTests Passed" << std::endl;
 }
 
