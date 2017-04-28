a117bc6ed3cb66a348f0399fd08353f546d317e1
skp2140 <skp2140@columbia.edu>
2017-04-27 23:50:11 -0400

    Tests for SVM added

--- tests/test_svm.cpp
+++ tests/test_svm.cpp
@@ -10,5 +10,16 @@
 
 void test_svm()
 {
+    mat X;
+    colvec Y;
+    read_csv("samples_csv/linear.csv", X, Y, true, ',', '#');
 
+    std::cout << "\nTESTING SVM" << std::endl;
+    Dataset data;
+    split_train_test(X, Y, data, 0.8);
+    SVM svm_clfr(0.1);
+    svm_clfr.train(data.Xtrain, data.Ytrain, 1000);
+    colvec preds_svm;
+    svm_clfr.predict(data.Xtest, preds_svm);
+    std::cout << "\nTests Passed" << std::endl;
 }
