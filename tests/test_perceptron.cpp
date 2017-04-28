fadcf11b955434ada769494a6e179e5f681542cc
skp2140 <skp2140@columbia.edu>
2017-04-27 23:56:48 -0400

    Tests for perceptron added

--- tests/test_perceptron.cpp
+++ tests/test_perceptron.cpp
@@ -10,5 +10,16 @@
 
 void test_perceptron()
 {
+    mat X;
+    colvec Y;
+    read_csv("samples_csv/linear.csv", X, Y, true, ',', '#');
 
+    std::cout << "\nTESTING PERCEPTRON" << std::endl;
+    Dataset data;
+    split_train_test(X, Y, data, 0.8);
+    Perceptron pcptr_clfr;
+    pcptr_clfr.train(data.Xtrain, data.Ytrain, 1000);
+    colvec preds_pcptr;
+    pcptr_clfr.predict(data.Xtest, preds_pcptr);
+    std::cout << "\nTests Passed" << std::endl;
 }
