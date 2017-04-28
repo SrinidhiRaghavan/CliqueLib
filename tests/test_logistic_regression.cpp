5d24d00ac35ffe8e05c4acfe7b4174bcde7384d3
skp2140 <skp2140@columbia.edu>
2017-04-27 23:43:16 -0400

    Tests for armadillo logistic regression added

--- tests/test_logistic_regression.cpp
+++ tests/test_logistic_regression.cpp
@@ -10,6 +10,18 @@
 
 void test_logistic_regression()
 {
+    mat X;
+    colvec Y;
+    read_csv("samples_csv/linear.csv", X, Y, true, ',', '#');
 
+    std::cout << "\nTESTING LOGISTIC REGRESSION" << std::endl;
+    Dataset data;
+    split_train_test(X, Y, data, 0.8);
+    Logistic logistic_clfr(0.01);
+    logistic_clfr.train(data.Xtrain, data.Ytrain, 100);
+    colvec preds_log;
+    logistic_clfr.predict(data.Xtest, preds_log);
+
+    std::cout << "\nTests Passed" << std::endl;
 }
 
