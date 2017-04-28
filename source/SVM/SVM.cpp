ba80ecb052d6f81dd459703c6e1908386fe05e15
skp2140 <skp2140@columbia.edu>
2017-04-27 22:02:54 -0400

    SVM.cpp format modified

--- source/SVM/SVM.cpp
+++ source/SVM/SVM.cpp
@@ -20,76 +20,70 @@ using namespace arma;
 //Constructor for the SVM
 SVM::SVM(double C_val) 
 {
-	C = C_val;
+    C = C_val;
 }
 
 
-//Implementation of the Fit function
+//Implementation of the train function
 void SVM::train(const mat& data, const colvec& Y, uword epoch) 
 {
 	
-	//n - Number of data entries in data
-	//d - Dimension of each entry in data
-	//L - Lambda parameter for SVM
-	//w - Weight vector including the bias 
+    //n - Number of data entries in data
+    //d - Dimension of each entry in data
+    //L - Lambda parameter for SVM
+    //w - Weight vector including the bias 
 	
-	uword n = size(data, 0);
-	uword d = size(data, 1);
-	double L = 1 / (n*C);
-	colvec w(d+1);
-	w.fill(0);
+    uword n = size(data, 0);
+    uword d = size(data, 1);
+    double L = 1 / (n*C);
+    colvec w(d+1);
+    w.fill(0);
+    
+    //Uniform Random Generator used for Generating the data entry indices randomly
+    default_random_engine generator;
+    uniform_int_distribution<int> distribution(0, n-1); 
 	
+    //Concatenating the matrix X with a column of 1's. Bias is the weight corresponding to this column
+    mat X = join_horiz(data, ones(n, 1));
 
-	//Uniform Random Generator used for Generating the data entry indices randomly
-	default_random_engine generator;
-	uniform_int_distribution<int> distribution(0, n-1);
-
-	
-	//Concatenating the matrix X with a column of 1's. Bias is the weight corresponding to this column
-	mat X = join_horiz(data, ones(n, 1));
-
-	//Uses Pegasos Algorithm for Linear Kernels to Find the Ideal Weight Vector for the Data
-	for (uword t = 0; t < epoch; t++) 
-        {
+    //Uses Pegasos Algorithm for Linear Kernels to Find the Ideal Weight Vector for the Data
+    for (uword t = 0; t < epoch; t++) 
+    {
 		
-		//nt - Learning rate which is dependent on Lambda and the epoch number 
-		//r  - Random number generated which chooses a particular data from X uniformly
-		//xt - rth row of X
-		//yt - rth value of Y		
+        //nt - Learning rate which is dependent on Lambda and the epoch number 
+	//r  - Random number generated which chooses a particular data from X uniformly
+	//xt - rth row of X
+	//yt - rth value of Y		
 
-		double nt = 1 / (L*(t+1));
-		uword r = distribution(generator);
-		rowvec xt = X.row(r);
-		double yt = Y(r);
-		double a = dot(w, xt)*yt;
-		if (a < 1.0)
-			w = (1 - nt*L)*w + nt*yt*xt.t();
-
-
-		else
-			w = (1 - nt*L)*w;
-		
-//		w = min(1.0, 1 / (sqrt(L)*norm(w)))*w;
-	}
-	weight = w;
+	double nt = 1 / (L*(t+1));
+	uword r = distribution(generator);
+	rowvec xt = X.row(r);
+	double yt = Y(r);
+	double a = dot(w, xt)*yt;
+	if (a < 1.0)
+	    w = (1 - nt*L)*w + nt*yt*xt.t();
+	else
+   	    w = (1 - nt*L)*w;
+    }
+    weight = w;
 }
 
 
 //Implementation of the predict function 
 void SVM::predict(const mat& data, colvec& Y) 
 {
-	//n - Number of data entries in data
-	uword n = size(data, 0);
+    //n - Number of data entries in data
+    uword n = size(data, 0);
     Y.ones(n, 1);
 	
-	//Concatenating the matrix X with a column of 1's. Bias is the weight corresponding to this column
-	mat X = join_horiz(data, ones(n, 1));
+    //Concatenating the matrix X with a column of 1's. Bias is the weight corresponding to this column
+    mat X = join_horiz(data, ones(n, 1));
 
-	//Making the Predictions
-	//Step 1 - Find the product of the data entries and the weight 
-	//Step 2 - If the product >= 0, then predict 1 else -1
+    //Making the Predictions
+    //Step 1 - Find the product of the data entries and the weight 
+    //Step 2 - If the product >= 0, then predict 1 else -1
 	
-	colvec A = X*weight;
+    colvec A = X*weight;
     uvec idx = find(A < 0);
     Y.elem(idx).fill(-1);
 }
