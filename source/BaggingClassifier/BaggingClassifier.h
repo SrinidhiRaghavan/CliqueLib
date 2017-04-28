2fa69a5cb2ba3bf18469a656237ac9534347beb3
skp2140 <skp2140@columbia.edu>
2017-04-27 20:49:36 -0400

    Doxygen for BaggingClassifier added

--- source/BaggingClassifier/BaggingClassifier.h
+++ source/BaggingClassifier/BaggingClassifier.h
@@ -23,14 +23,36 @@ template<Classifier C>
 class BaggingClassifier 
 {
     private:
-	std::vector<std::shared_ptr<C>> base_estimators;
+	std::vector<std::shared_ptr<C>> base_estimators; /*!< base_estimators represents the number of classifiers
+                                                              in an ensemble.
+                                                          */
 	arma::uword n_estimators;
 	arma::uword max_samples;
 
     public:
-	BaggingClassifier(arma::uword, arma::uword);
-	void train(const arma::mat&, const arma::colvec&, arma::uword);
-	void predict(const arma::mat&, arma::colvec&);
+	BaggingClassifier(arma::uword, arma::uword); /**< The BaggingClassifier object contains two  main variables, the
+                                                          first of which represents the number of estimators and is
+                                                          accompanied with max samples.
+                                                      */
+
+
+	void train(const arma::mat&, const arma::colvec&, arma::uword); /**< The train function accepts a matrix of
+                                                                             sample inputs, a column vector of sample
+                                                                             labels, and a uword representing the number
+                                                                             of epochs. Given these inputs, the bagging 
+                                                                             classifier iterates over the number of
+                                                                             estimators and calls the train functions of
+                                                                             corresponding algorithms. As this is bagging,
+                                                                             the sets are randomized and models are
+                                                                             inserted into base_estimators
+                                                                         */
+
+	void predict(const arma::mat&, arma::colvec&); /**< The predict function iterates over base_estimators, which
+                                                            contains all the classifiers in the ensemble, and predicts 
+                                                            on each model in the ensemble. Consequently, given the
+                                                            predictions from all models, the ensemble combines the 
+                                                            outputs and yields a final vector of labels. 
+                                                        */
 };
 
 #endif
